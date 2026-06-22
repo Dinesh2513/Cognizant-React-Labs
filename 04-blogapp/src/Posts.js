b6c1dfc126ef73ebdad7995145f7a92baf6fa21d
Dinesh <dineshkalyani1304@gmail.com>
2026-06-22 10:43:00 +0530

    Add fourth React lab blogapp

---
+++ 04-blogapp/src/Posts.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        const posts = data.map(
+          post => new Post(post.id, post.title, post.body)
+        );
+
+        this.setState({
+          posts: posts
+        });
+      })
+      .catch(error => {
+        alert('Error while fetching posts: ' + error);
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert('Error occurred: ' + error);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
