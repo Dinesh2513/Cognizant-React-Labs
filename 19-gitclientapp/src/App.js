cd88bba73dd6d905c62bff2474f378994a92e9ec
Dinesh <dineshkalyani1304@gmail.com>
2026-06-25 15:08:01 +0530

    Add nineteenth React lab git client app

---
+++ 19-gitclientapp/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import GitClient from './GitClient';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    GitClient.getRepositories('techiesyed')
+      .then((response) => {
+        setRepositories(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Git repositories of User - TechieSyed</h1>
+
+      {repositories.map((repository) => (
+        <p key={repository.name}>{repository.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ 19-gitclientapp/src/GitClient.test.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import GitClient from './GitClient';
+
+jest.mock('axios');
+
+describe('Git Client Tests', () => {
+  test('should return repository names for techiesyed', async () => {
+    const repositories = [
+      { name: 'ReactJS-HOL' },
+      { name: 'Angular-HOL' },
+      { name: 'Microservices-HOL' }
+    ];
+
+    axios.get.mockResolvedValue({
+      data: repositories
+    });
+
+    const response = await GitClient.getRepositories('techiesyed');
+
+    expect(response.data).toEqual(repositories);
+    expect(response.data[0].name).toBe('ReactJS-HOL');
+    expect(axios.get).toHaveBeenCalledWith(
+      'https://api.github.com/users/techiesyed/repos'
+    );
+  });
+});
